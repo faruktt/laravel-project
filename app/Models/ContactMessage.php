f19b5f1b1b66136e2ae2d9f8ff5810a1f5428b1d
Faruk Hossain <faruktt143@gmail.com>
2025-09-26 12:09:30 +0600

    update

--- app/Models/ContactMessage.php
+++ app/Models/ContactMessage.php
@@ -5,9 +5,4 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class ContactMessage extends Model
-{
 
-    protected $table = 'contact_messages';
-
-    protected $fillable = ['name', 'email', 'subject', 'message'];
-}
